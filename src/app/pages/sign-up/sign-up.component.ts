8d1909056ebd935afb7f252b8b20ef682d3ee2f2
Laura Sofia Chaves <sofita92004@gmail.com>
2024-05-12 10:08:54 -0500

    add changes

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule, HttpClientModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
